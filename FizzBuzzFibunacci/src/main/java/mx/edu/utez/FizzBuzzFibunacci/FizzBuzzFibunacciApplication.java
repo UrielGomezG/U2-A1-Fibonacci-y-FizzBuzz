8cf1c1f9e8ec0960f1b3db4a205efc8e90c0902c
AldahirG <20173ti173@utez.edu.mx>
2026-09-24 13:17:35 -0600

    repo

---
+++ FizzBuzzFibunacci/src/main/java/mx/edu/utez/FizzBuzzFibunacci/FizzBuzzFibunacciApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.FizzBuzzFibunacci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzzFibunacciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzBuzzFibunacciApplication.class, args);
+	}
+
+}
